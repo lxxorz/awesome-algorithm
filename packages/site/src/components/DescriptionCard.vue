aaa0329d5889400f945d5f8f31e38c3bd5e1ee61
bjorn <lxxorz@gmail.com>
2023-02-25 17:39:09 +0800

    add math block desc

--- packages/site/src/components/DescriptionCard.vue
+++ packages/site/src/components/DescriptionCard.vue
@@ -1,18 +1,8 @@
 <template>
   <n-card style="overflow:auto">
     <div class="card">
-      选择排序（英语：Selection sort）是一种简单直观的排序算法。它的工作原理是每次找出第 i 小的元素（也就是<span class="desc"></span>中最小的元素），然后将这个元素与数组第 i
-      个位置上的元素交换。Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum perferendis tempore ut. Sit, maiores
-      culpa? Possimus cumque distinctio vitae nobis necessitatibus. Ratione placeat dolor omnis illo similique,
-      voluptatum aliquid optio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quia reiciendis
-      obcaecati culpa quaerat doloremque corporis ipsa nobis officia vero qui quo sint sit, suscipit aliquam. Est porro
-      facere accusamus! Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, explicabo cum voluptatibus
-      consequuntur impedit ipsam minima, delectus est sed accusamus sequi nam laborum exercitationem dolorem ratione
-      iste omnis perferendis tempore? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat mollitia dolorem
-      rerum natus repellendus, sunt quibusdam maxime nisi omnis illo in quae dolor, obcaecati enim excepturi est
-      incidunt quaerat vero! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut expedita labore beatae?
-      Doloribus quia voluptatibus laudantium iusto sint perspiciatis quas, soluta, modi dolor exercitationem unde totam.
-      Exercitationem nulla nihil at.
+      <span class="desc" v-html="rendered_desc.value">
+      </span>
     </div>
 
   </n-card>
@@ -20,23 +10,15 @@
 
 <script setup lang="ts">
 import { NCard } from 'naive-ui';
-import { onMounted, nextTick } from 'vue';
-import renderMathInElement from "katex"
-onMounted(() => {
-  nextTick(() => {
-    const element: HTMLElement = document.querySelector(".desc")!;
-    renderMathInElement.render(String.raw`f(x)=2x^2 `, element, {
-      "displayMode": false,
-      "output": "mathml"
-    })
-  })
-})
+import { ref, computed, watchEffect, render } from 'vue';
+import { useMathScope } from '@/utils/common';
+let desc = ref(String.raw`some thing about $E=mc^2$ and $f(x)=\sqrt{a+b}$`);
+const rendered_desc = computed(() => useMathScope(desc));
 </script>
 
 <style scoped>
 .card {
   max-height: 100%;
   font-size: 1.1rem;
-  color: #404040;
 }
 </style>
--- packages/site/src/utils/common.ts
+++ packages/site/src/utils/common.ts
@@ -1,4 +1,6 @@
-import { h, type Component } from "vue"
+import { h, ref, type Component } from "vue"
+import type { MaybeRef } from "@vueuse/core";
+import renderMathInElement from "katex"
 import { NIcon } from "naive-ui";
 export const randomArray = (arr: number[]) => {
   for (let i = arr.length - 1; i >= 0; --i) {
@@ -17,4 +19,20 @@ export async function sleep(delay: number) {
 
 export function renderIcon(icon: Component) {
   return () => h(NIcon, null, { default: () => h(icon) });
+}
+
+export function useMathScope(desc: MaybeRef<string>) {
+  const inline_math_reg = /\$[^$]+?\$/g
+  desc = ref(desc)
+  let match = inline_math_reg.exec(desc.value);
+  while (match != null) {
+    console.log("match", match[0]);
+    desc.value = desc.value.replace(match[0], renderMathInElement.renderToString(match[0].replaceAll("$", ""), {
+      displayMode: false,
+      output: "mathml"
+    }))
+    match = inline_math_reg.exec(desc.value);
+  }
+  console.log(desc.value);
+  return desc;
 }
